212d3cdd6613d2e2ef8a4085178fd56ea925edde
Matt Tedder <mtedder@Techtonics-MacBook-Pro-2.local>
2019-09-24 11:21:30 -0600

    adds to test ability to mark newly created todo as done

--- cypress/integration/todomvc.spec.js
+++ cypress/integration/todomvc.spec.js
@@ -5,8 +5,10 @@ it('should navigate to the TodoMvC App', () => {
 })
 
 
-it('should be able to add a new todo to the list', () => {
+it('should be able to add a new todo to the list and mark it as complete', () => {
     cy.visit('http://todomvc-app-for-testing.surge.sh/?delay-new-todo=5000')
 
     cy.get('.new-todo', {timeout: 6000}).type("Clean room{enter}") // command chaining
+
+    cy.get('.toggle').click()
 })
