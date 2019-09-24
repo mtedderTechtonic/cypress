4f8dc71342efe937c6b90fc3ef2365f5377031c7
Matt Tedder <mtedder@Techtonics-MacBook-Pro-2.local>
2019-09-24 10:38:17 -0600

    installs cypress, writes first test

---
+++ cypress/integration/todomvc.spec.js
@@ -0,0 +1,12 @@
+/// <reference types="cypress" />
+
+it('should navigate to the TodoMvC App', () => {
+    cy.visit('http://todomvc-app-for-testing.surge.sh/')
+})
+
+
+it('should be able to add a new todo to the list', () => {
+    cy.visit('http://todomvc-app-for-testing.surge.sh/?delay-new-todo=5000')
+
+    cy.get('.new-todo', {timeout: 6000}).type("Clean room{enter}") // command chaining
+})
