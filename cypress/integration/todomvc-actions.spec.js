eaf6ede9a92afddee939cb6ccaf6ed2b1db703c9
Matt Tedder <mtedder@Techtonics-MacBook-Pro-2.local>
2019-09-25 13:56:44 -0600

    Separates tests by actions performed

--- cypress/integration/todomvc-actions.spec.js
+++ cypress/integration/todomvc-actions.spec.js
@@ -1,30 +1,34 @@
 /// <reference types="cypress" />
 
-describe('Cypress Documentation', () => {
-    it('Does not do much!', () => {
+describe('cypress documentation', () => {
+    it('does not do much!', () => {
         expect(true).to.equal(true)
     })
 })
 
-describe('TAU Course Tests', () => {
-    it('should navigate to the TodoMvC App', () => {
+describe('todo actions', () => {
+    beforeEach(() => {
         cy.visit('http://todomvc-app-for-testing.surge.sh/')
-    })
-
-    it('should be able to add a new todo to the list and mark it as complete', () => {
-        cy.visit('http://todomvc-app-for-testing.surge.sh/')
-    
         cy.get('.new-todo', {timeout: 3000}).type("Clean room{enter}") // command chaining
-
+    })
+    it('should add a new todo to the list', () => {
+        
         cy.get('label').should('have.text', 'Clean room') // use cypress' should method to validate tests
         cy.get('.toggle').should('not.be.checked')
-
+    })
+    
+    it('should mark a todo as completed', () => {
         cy.get('.toggle').click()
         cy.get('label').should('have.css', 'text-decoration-line', 'line-through') // checks css after action is performed
-    
+    })
+
+       
+    it('should clear completed todos', () => {
+        cy.get('.toggle').click()
+
         cy.contains('Clear').click()
 
         cy.get('.todo-list').should('not.have.descendants', 'li') // validates whether list is cleared after clicking clear list
+        
     })
-
 })
