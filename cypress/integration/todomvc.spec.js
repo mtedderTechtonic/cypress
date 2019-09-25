f0a4d744100bc2b22446f135d6d28b27b88231ab
Matt Tedder <mtedder@Techtonics-MacBook-Pro-2.local>
2019-09-25 08:32:52 -0600

    Adds validations for tests that were written

--- cypress/integration/todomvc.spec.js
+++ cypress/integration/todomvc.spec.js
@@ -1,14 +1,30 @@
 /// <reference types="cypress" />
 
-it('should navigate to the TodoMvC App', () => {
-    cy.visit('http://todomvc-app-for-testing.surge.sh/')
+describe('Cypress Documentation', () => {
+    it('Does not do much!', () => {
+        expect(true).to.equal(true)
+    })
 })
 
+describe('TAU Course Tests', () => {
+    it('should navigate to the TodoMvC App', () => {
+        cy.visit('http://todomvc-app-for-testing.surge.sh/')
+    })
 
-it('should be able to add a new todo to the list and mark it as complete', () => {
-    cy.visit('http://todomvc-app-for-testing.surge.sh/?delay-new-todo=5000')
+    it('should be able to add a new todo to the list and mark it as complete', () => {
+        cy.visit('http://todomvc-app-for-testing.surge.sh/')
+    
+        cy.get('.new-todo', {timeout: 3000}).type("Clean room{enter}") // command chaining
 
-    cy.get('.new-todo', {timeout: 6000}).type("Clean room{enter}") // command chaining
+        cy.get('label').should('have.text', 'Clean room') // use cypress' should method to validate tests
+        cy.get('.toggle').should('not.be.checked')
 
-    cy.get('.toggle').click()
+        cy.get('.toggle').click()
+        cy.get('label').should('have.css', 'text-decoration-line', 'line-through') // checks css after action is performed
+    
+        cy.contains('Clear').click()
+
+        cy.get('.todo-list').should('not.have.descendants', 'li') // validates whether list is cleared after clicking clear list
+    })
+
 })
