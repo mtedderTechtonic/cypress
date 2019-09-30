41e43f6aab4d9e3400ea8ecef6b148e8abfca513
Matt Tedder <mtedder@Techtonics-MacBook-Pro-2.local>
2019-09-30 13:33:04 -0600

    Adds applitools eyes to verify different dimensions

--- cypress/integration/todomvc-visuals.spec.js
+++ cypress/integration/todomvc-visuals.spec.js
@@ -1,13 +1,36 @@
 /// <reference types="cypress" />
-import * as todoPage from '../page-objects/todo-page'
+import * as todoPage from '../page-objects/todo-page';
+import { each } from 'cypress/types/bluebird';
 
-describe('visual validation', () => { // mocha.js preamble
-    before(() => todoPage.navigate())
+describe('visual validation', () => {
+	// mocha.js preamble
+	before(() => todoPage.navigate());
 
-    it('should look good', () => { // test function
-        todoPage.addToDo('Clean room')
-        todoPage.addToDo('Learn JavaScript')
+	beforeEach(() =>
+		cy.eyesOpen({
+			appName: 'TAU TodoMVC',
+			batchName: 'TAU TodoMVC Hey!',
+			browser: [
+				{ name: 'chrome', width: 1024, height: 768 },
+				{ name: 'chrome', width: 800, height: 600 },
+				{ name: 'firefox', width: 1024, height: 768 },
+				{ deviceName: 'iPhone X' }
+			]
+		})
+	);
+	afterEach(() => cy.eyesClose());
 
-        todoPage.toggleTodo()
-    })
-})
+	it('should look good', () => {
+		// test function
+		cy.eyesCheckWindow('empty todo list');
+
+		todoPage.addToDo('Clean room');
+		todoPage.addToDo('Learn JavaScript');
+
+		cy.eyesCheckWindow('two todos');
+
+		todoPage.toggleTodo();
+
+		cy.eyesCheckWindow('mark as completed');
+	});
+});
